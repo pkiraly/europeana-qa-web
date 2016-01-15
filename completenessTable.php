75d48975f13ad183e38a1b8c84ae2370ee561a74
Peter Kiraly <peter.kiraly@gwdg.de>
2016-01-15 08:49:26 +0100

    Initial commit.

---
+++ completenessTable.php
@@ -0,0 +1,153 @@
+<?php
+define('LN', "\n");
+define('OAI_PMH_URL_TEMPLATE', 'http://oai.europeana.eu/oaicat/OAIHandler?verb=GetRecord&metadataPrefix=edm&identifier=%s');
+define('ID_PREFIX', 'http://data.europeana.eu/item/');
+
+$configuration = parse_ini_file('config.cfg');
+require_once($configuration['OAI_PATH'] . '/OAIHarvester.php');
+
+$id = isset($_GET['id']) ? $_GET['id'] : $argv[1];
+if (strpos($id, ID_PREFIX) !== FALSE) {
+  $id = str_replace(ID_PREFIX, '', $id);
+}
+if (substr($id, 0, 1) == '/') {
+  $id = substr($id, 1);
+}
+
+$params = array(
+  'metadataPrefix' => 'edm',
+  'identifier' => ID_PREFIX . $id
+);
+
+$harvester = new OAIHarvester('GetRecord', 'http://oai.europeana.eu/oaicat/OAIHandler', $params);
+$harvester->fetchContent();
+$harvester->processContent();
+if (($record = $harvester->getNextRecord()) != null) {
+  $metadata = processRecord($record);
+  if (!empty($metadata)) {
+    $command = sprintf(
+      "java -jar %s/europeana-qa-1.0-SNAPSHOT-jar-with-dependencies.jar '%s' 2>/dev/null",
+      $configuration['JAR_PATH'],
+      str_replace("'", "'\''", json_encode($metadata))
+    );
+    $analysis = json_decode(exec($command));
+  }
+}
+
+$graphs = array(
+  'total' => array('label' => 'Completeness', 'fields' => array("edm:ProvidedCHO/@about", "Proxy/dc:title", "Proxy/dcterms:alternative",
+  	"Proxy/dc:description", "Proxy/dc:creator", "Proxy/dc:publisher", "Proxy/dc:contributor", "Proxy/dc:type", "Proxy/dc:identifier",
+  	"Proxy/dc:language", "Proxy/dc:coverage", "Proxy/dcterms:temporal", "Proxy/dcterms:spatial", "Proxy/dc:subject", "Proxy/dc:date",
+  	"Proxy/dcterms:created", "Proxy/dcterms:issued", "Proxy/dcterms:extent", "Proxy/dcterms:medium", "Proxy/dcterms:provenance",
+  	"Proxy/dcterms:hasPart", "Proxy/dcterms:isPartOf", "Proxy/dc:format", "Proxy/dc:source", "Proxy/dc:rights", "Proxy/dc:relation",
+  	"Proxy/edm:isNextInSequence", "Proxy/edm:type", "Proxy/edm:rights", "Aggregation/edm:rights", "Aggregation/edm:provider",
+  	"Aggregation/edm:dataProvider", "Aggregation/edm:isShownAt", "Aggregation/edm:isShownBy", "Aggregation/edm:object", "Aggregation/edm:hasView")),
+  'mandatory' => array('label' => 'Mandatory elements', 'fields' => array("edm:ProvidedCHO/@about", "Proxy/dc:title", "Proxy/dc:description",
+  	"Proxy/dc:type", "Proxy/dc:coverage", "Proxy/dcterms:spatial", "Proxy/dc:subject", "Proxy/edm:rights", "Aggregation/edm:rights",
+  	"Aggregation/edm:provider", "Aggregation/edm:dataProvider", "Aggregation/edm:isShownAt", "Aggregation/edm:isShownBy")),
+  'descriptiveness' => array('label' => 'Descriptiveness', 'fields' => array("Proxy/dc:title", "Proxy/dcterms:alternative", "Proxy/dc:description",
+  	"Proxy/dc:creator", "Proxy/dc:language", "Proxy/dc:subject", "Proxy/dcterms:extent", "Proxy/dcterms:medium", "Proxy/dcterms:provenance",
+  	"Proxy/dc:format", "Proxy/dc:source")),
+  'searchability' => array('label' => 'Searchability', 'fields' => array("Proxy/dc:title", "Proxy/dcterms:alternative", "Proxy/dc:description", 
+  	"Proxy/dc:creator", "Proxy/dc:publisher", "Proxy/dc:contributor", "Proxy/dc:type", "Proxy/dc:coverage", "Proxy/dcterms:temporal", 
+  	"Proxy/dcterms:spatial", "Proxy/dc:subject", "Proxy/dcterms:hasPart", "Proxy/dcterms:isPartOf", "Proxy/dc:relation", "Proxy/edm:isNextInSequence",
+  	"Proxy/edm:type", "Aggregation/edm:provider", "Aggregation/edm:dataProvider")),
+  'contextualization' => array('label' => 'Contextualization', 'fields' => array("Proxy/dc:description", "Proxy/dc:creator", "Proxy/dc:type",
+  	"Proxy/dc:coverage", "Proxy/dcterms:temporal", "Proxy/dcterms:spatial", "Proxy/dc:subject", "Proxy/dcterms:hasPart", "Proxy/dcterms:isPartOf",
+  	"Proxy/dc:relation", "Proxy/edm:isNextInSequence")),
+  'identification' => array('label' => 'Identification', 'fields' => array("Proxy/dc:title", "Proxy/dcterms:alternative", "Proxy/dc:description",
+  	"Proxy/dc:type", "Proxy/dc:identifier", "Proxy/dc:date", "Proxy/dcterms:created", "Proxy/dcterms:issued", "Aggregation/edm:provider",
+  	"Aggregation/edm:dataProvider")),
+  'browsing' => array('label' => 'Browsing', 'fields' => array("Proxy/dc:creator", "Proxy/dc:type", "Proxy/dc:coverage", "Proxy/dcterms:temporal",
+  	"Proxy/dcterms:spatial", "Proxy/dc:date", "Proxy/dcterms:hasPart", "Proxy/dcterms:isPartOf", "Proxy/dc:relation", "Proxy/edm:isNextInSequence",
+  	"Proxy/edm:type", "Aggregation/edm:isShownAt", "Aggregation/edm:isShownBy", "Aggregation/edm:hasView")),
+  'viewing' => array('label' => 'Viewing', 'fields' => array("Aggregation/edm:isShownAt", "Aggregation/edm:isShownBy", "Aggregation/edm:object",
+  	"Aggregation/edm:hasView")),
+  'reusability' => array('label' => 'Re-usability', 'fields' => array("Proxy/dc:publisher", "Proxy/dc:date", "Proxy/dcterms:created",
+  	"Proxy/dcterms:issued", "Proxy/dcterms:extent", "Proxy/dcterms:medium", "Proxy/dc:format", "Proxy/dc:rights", "Proxy/edm:rights",
+  	"Aggregation/edm:rights", "Aggregation/edm:isShownBy", "Aggregation/edm:object")),
+  'multilinguality' => array('label' => 'Multilinguality', 'fields' => array("Proxy/dc:title", "Proxy/dcterms:alternative", "Proxy/dc:description",
+  	"Proxy/dc:language", "Proxy/dc:subject"))
+);
+
+$table = array();
+$table[0] = array();
+foreach ($graphs as $key => $object) {
+  $table[0][] = $key == 'total' ? '&nbsp;' : $object['label'];
+}
+
+foreach ($graphs['total']['fields'] as $field) {
+  $row = array($field);
+  $color = in_array($field, $analysis->existingFields) ? 'green' : 'grey';
+  // $color = $colors[rand(0, 2)];
+  foreach ($graphs as $key => $object) {
+    if ($key == 'total')
+      continue;
+    if (in_array($field, $object['fields'])) {
+      if ($key == 'mandatory' && $color != 'green')
+        $row[] = 'red';
+      else
+        $row[] = $color;
+    } else {
+      $row[] = '';
+    }
+  }
+  $table[] = $row;
+}
+
+include("completenessTable.tpl.php");
+
+// echo json_encode(json_decode(file_get_contents("http://www.europeana.eu/portal/record/11620/MNHNBOTANY_MNHN_FRANCE_P04617748.json")), JSON_PRETTY_PRINT);
+
+
+function processRecord($record) {
+  $metadata = array();
+  $isDeleted = (isset($record['header']['@status']) && $record['header']['@status'] == 'deleted');
+  $id = $record['header']['identifier'];
+  if (!$isDeleted) {
+    $metadata = dom_to_array($record['metadata']['childNode']);
+    $metadata['qIdentifier'] = $record['header']['identifier'];
+    $metadata['identifier'] = str_replace(ID_PREFIX, '', $record['header']['identifier']);
+    $metadata['sets'] = $record['header']['setSpec'];
+  }
+  return $metadata;
+}
+
+/**
+ * Transform DOM object to an array
+ */
+function dom_to_array($node, $parent_name = NULL) {
+  $name = $node->nodeName;
+  $metadata = array();
+
+  // copy attributes
+  foreach ($node->attributes as $attr) {
+    $metadata['@' . $attr->name] = $attr->value;
+  }
+
+  // process children
+  foreach ($node->childNodes as $child) {
+    // process children elements
+    if ($child->nodeType == XML_ELEMENT_NODE) {
+      if ($node->childNodes->length == 1) {
+        $metadata[$child->nodeName] = dom_to_array($child, $name);
+      } else {
+        $metadata[$child->nodeName][] = dom_to_array($child, $name);
+      }
+    }
+    // copy text value
+    elseif ($child->nodeType == XML_TEXT_NODE) {
+      $value = trim($child->nodeValue);
+      if (!empty($value)) {
+        $metadata['#value'] = str_replace("\n", ' ', $value);
+      }
+    }
+  }
+  if ($parent_name !== NULL) {
+    if (count($metadata) == 1 && isset($metadata['#value'])) {
+      $metadata = $metadata['#value'];
+    }
+  }
+
+  return $metadata;
+}
---
+++ completenessTable.tpl.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
+  <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Completeness</title>
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/styles/default.min.css" />
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway" type="text/css" />
+<link rel="stylesheet" href="completenessTable.css" />
+</head>
+<body>
+
+<div class="page-header">
+  <h1>Record investigation</h1>
+  <h3><a href="index.html">Metadata Quality Assurance Framework</a></h3>
+  </div>
+</div>
+
+<div class="col-md-12">
+  <p>The overview of the "completeness" metrics of a Europeana record. You can check if the record has the most important fields, which are important for the different functionalities at the Europeana database. The functionalities:</p>
+  <ul type="square">
+    <li>Descriptiveness &ndash; how much information has the metadata to describe what the object is about</li>
+    <li>Searchability &ndash; the fields most heavily used in searches</li>
+    <li>Contextualization &ndash; the bases for finding connected entities (persons, places, times, etc.) in the record</li>
+    <li>Identification &ndash; for unambiguously identifying the object</li>
+    <li>Browsing &ndash; for the browsing features at the portal</li>
+    <li>Viewing &ndash; for the displaying at the portal</li>
+    <li>Re-usability &ndash; for reusing the metadata records in other systems</li>
+    <li>Multilinguality &ndash; for multilingual aspects, to be understandable for all European citizen</li>
+  </ul>
+  <p>Above those functionalities, we measure the mandatory element, and a collection of all the fields covered in these functionalities.</p>
+
+
+  <p>Check a specific Europeana record by entering its ID.</p>
+  <form>
+    <input type="text" name="id" value="<?php print $id ?>" size="120" />
+    <input type="submit" value="Check ID"/>
+  </form>
+</div>
+
+<div class="col-md-5">
+<h2>Completeness of elements & functionalities</h2>
+
+<table id="fields">
+  <thead>
+    <tr>
+<?php foreach ($table[0] as $key) { ?>
+      <th><div class="functionality"><span><?php print $key; ?></span></div></th>
+<?php } ?>
+    </tr>
+  </thead>
+  <tbody>
+<?php foreach ($table as $key => $row) { ?>
+<?php if ($key == 0) continue; ?>
+    <tr>
+<?php foreach ($row as $j => $cell) { ?>
+<?php if ($j == 0) { ?>
+      <td class="field"><?php print $cell; ?></td>
+<?php } elseif ($cell == '') { ?>
+      <td>&nbsp;</td>
+<?php } else { ?>
+      <td class="<?php print $cell; ?>">&nbsp;</td>
+<?php } ?>
+<?php } ?>
+    </tr>
+<?php } ?>
+  </tbody>
+</table>
+
+<table id="legend" class="table">
+  <thead>
+    <tr>
+      <th></th>
+      <th>Explanation of colors</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="green">&nbsp;</td>
+      <td>existing element</td>
+    </tr>
+    <tr>
+      <td class="grey">&nbsp;</td>
+      <td>missing element</td>
+    </tr>
+    <tr>
+      <td class="red">&nbsp;</td>
+      <td>missing mandatory element</td>
+    </tr>
+  </tbody>
+</table>
+
+<h2>Statistics</h2>
+<table id="statistics" class="table table-bordered table-striped">
+  <thead>
+    <tr>
+      <th>Functionality</th>
+      <th>Score</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php foreach ($graphs as $key => $object) { ?>
+    <tr>
+      <td><?php print $object['label']; ?></td>
+      <td><?php print $analysis->results->{strtoupper($key)}; ?></td>
+    </tr>
+<?php } ?>
+  </tbody>
+</table>
+
+</div>
+
+<div class="col-md-7">
+<h2>Available representations</h2>
+<p></p>
+<ul type="square">
+  <li>Canonical identifier: <a href="http://data.europeana.eu/item/<?php print $id; ?>" target="_blank">http://data.europeana.eu/item/<?php print $id; ?></a></li>
+  <li><a href="http://europeana.eu/portal/record/<?php print $id ?>.html" target="_blank">Europeana portal</a></li>
+  <li><a href="http://www.europeana.eu/api/v2/record/<?php print $id ?>.json?wskey=api2demo" target="_blank">REST API: full record</a></li>
+  <li><a href="http://www.europeana.eu/api/v2/search.json?query=europeana_id:%22/<?php print $id ?>%22&wskey=api2demo" target="_blank">REST API: search record</a></li>
+  <li><a href="http://oai.europeana.eu/oaicat/OAIHandler?verb=GetRecord&metadataPrefix=edm&identifier=http://data.europeana.eu/item/<?php print $id; ?>" target="_blank">OAI-PMH server</a></li>
+</ul>
+
+<h2>Metadata structure as represented in the OAI-PMH service</h2>
+<pre id="code"><code class="json"><?php print json_encode($metadata, JSON_PRETTY_PRINT); ?></code></pre>
+</div>
+
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.1.0/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
+</body>
+</html>
