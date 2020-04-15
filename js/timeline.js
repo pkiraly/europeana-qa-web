7d29e4969bebb222de7e7ea8cdb6cf06e42dacac
Peter Kiraly <peter.kiraly@gwdg.de>
2020-04-15 09:05:30 +0200

    Add timeline

--- js/timeline.js
+++ js/timeline.js
@@ -1,7 +1,8 @@
+var timeline_w = 500;
+var timeline_h = 100;
+var timeline_barPadding = 2;
+
 function startInteractiveTimeline(targetId, tableClass) {
-  var timeline_w = 500;
-  var timeline_h = 100;
-  var timeline_barPadding = 2;
 
   var svg = d3.select('div#' + targetId)
               .append("svg")
@@ -45,52 +46,58 @@ function startInteractiveTimeline(targetId, tableClass) {
     })
 
     $('div#' + targetId + " svg").children().each(function(e) {$(this).remove()})
-    var max = d3.max(dataset, d => d.value);
-    var min = d3.min(dataset, d => d.value);
-    var range = max - min;
-    var minmaxPadding = (range != 0) ? range / 4 : 1.0;
 
-    var yScale = d3.scale.linear()
-      // .domain([min - minmaxPadding, max + minmaxPadding])
-      // .domain([min, max])
-      .domain([0, max])
-      .range([0, timeline_h]);
-
-    svg.selectAll("rect")
-       .data(dataset)
-       .enter()
-       .append("rect")
-       .attr("x", function(d, i) {
-         return i * (timeline_w / dataset.length);
-       })
-       .attr("y", function(d) {
-         return timeline_h - yScale(d.value);
-       })
-       .attr("width", timeline_w / dataset.length - timeline_barPadding)
-       .attr("height", function(d) {
-         return yScale(d.value);
-       })
-       .attr('fill', '#666')
-    ;
-
-    svg.selectAll("text")
-       .data(dataset)
-       .enter()
-       .append("text")
-       .text(function(d) {
-         return Math.ceil(d.value * 1000) / 1000;
-       })
-       .attr("x", function(d, i) {
-         return i * (timeline_w / dataset.length) + (timeline_w / dataset.length - timeline_barPadding) / 2;
-       })
-       .attr("y", function(d) {
-         return timeline_h - yScale(d.value) + 14;
-       })
-       .attr("font-family", "sans-serif")
-       .attr("font-size", "11px")
-       .attr("fill", "white")
-       .attr("text-anchor", "middle")
-    ;
+    drawBarchart(svg, dataset);
 
   });
+}
+
+function drawBarchart(svg, dataset) {
+  var max = d3.max(dataset, d => d.value);
+  var min = d3.min(dataset, d => d.value);
+  var range = max - min;
+  var minmaxPadding = (range != 0) ? range / 4 : 1.0;
+
+  var yScale = d3.scale.linear()
+  // .domain([min - minmaxPadding, max + minmaxPadding])
+  // .domain([min, max])
+  .domain([0, max])
+  .range([0, timeline_h]);
+
+  svg.selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("x", function(d, i) {
+    return i * (timeline_w / dataset.length);
+  })
+  .attr("y", function(d) {
+    return timeline_h - yScale(d.value);
+  })
+  .attr("width", timeline_w / dataset.length - timeline_barPadding)
+  .attr("height", function(d) {
+    return yScale(d.value);
+  })
+  .attr('fill', '#666')
+  ;
+
+  svg.selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function(d) {
+    return Math.ceil(d.value * 1000) / 1000;
+  })
+  .attr("x", function(d, i) {
+    return i * (timeline_w / dataset.length) + (timeline_w / dataset.length - timeline_barPadding) / 2;
+  })
+  .attr("y", function(d) {
+    return timeline_h - yScale(d.value) + 14;
+  })
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "11px")
+  .attr("fill", "white")
+  .attr("text-anchor", "middle")
+  ;
+
 }
