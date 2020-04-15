fee088224914f8ee79f5be5ce69601318adcf869
Peter Kiraly <peter.kiraly@gwdg.de>
2020-04-15 10:28:30 +0200

    Add timeline

--- js/timeline.js
+++ js/timeline.js
@@ -115,7 +115,7 @@ function drawLinechart(svg, dataset) {
             .range([0, timeline_w]);
 
   svg.append("g")
-     .attr("transform", "translate(20," + timeline_h + ")")
+     .attr("transform", "translate(0," + timeline_h + ")")
      .call(d3.axisBottom(x));
 
   // Add Y axis
