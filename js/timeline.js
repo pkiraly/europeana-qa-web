765d88d1f14daf19e8c40821397520d889097c2c
Peter Kiraly <peter.kiraly@gwdg.de>
2020-04-15 08:46:45 +0200

    Add timeline

--- js/timeline.js
+++ js/timeline.js
@@ -1,13 +1,13 @@
+var timeline_w = 500;
+var timeline_h = 100;
+var timeline_barPadding = 2;
+
+var svg = d3.select('div#' + targetId)
+            .append("svg")
+            .attr("width", timeline_w)
+            .attr("height", timeline_h);
+
 function startInteractiveTimeline(targetId, tableClass) {
-  var timeline_w = 500;
-  var timeline_h = 100;
-  var timeline_barPadding = 2;
-
-  var svg = d3.select('div#' + targetId)
-  .append("svg")
-  .attr("width", timeline_w)
-  .attr("height", timeline_h);
-
   var dataset = [];
   $('table.' + tableClass + ' td.property').on('click', function(e) {
     $('table.' + tableClass + ' td.highlighted').each(function(e) {
@@ -35,7 +35,6 @@ function startInteractiveTimeline(targetId, tableClass) {
         $(this).addClass('highlighted');
       }
     });
-    console.log(dataset);
     oTarget.animate({"top": (y - 510) + 'px', "left": x + 'px'}, "fast");
     oTarget.on('click', function() {
       oTarget.hide(1000);
@@ -45,52 +44,57 @@ function startInteractiveTimeline(targetId, tableClass) {
     })
 
     $('div#' + targetId + " svg").children().each(function(e) {$(this).remove()})
-    var max = d3.max(dataset, d => d.value);
-    var min = d3.min(dataset, d => d.value);
-    var range = max - min;
-    var minmaxPadding = (range != 0) ? range / 4 : 1.0;
 
-    var yScale = d3.scale.linear()
-      // .domain([min - minmaxPadding, max + minmaxPadding])
-      // .domain([min, max])
-      .domain([0, max])
-      .range([0, timeline_h]);
-
-    svg.selectAll("rect")
-       .data(dataset)
-       .enter()
-       .append("rect")
-       .attr("x", function(d, i) {
-         return i * (timeline_w / dataset.length);
-       })
-       .attr("y", function(d) {
-         return timeline_h - yScale(d.value);
-       })
-       .attr("width", timeline_w / dataset.length - timeline_barPadding)
-       .attr("height", function(d) {
-         return yScale(d.value);
-       })
-       .attr('fill', '#666')
-    ;
-
-    svg.selectAll("text")
-       .data(dataset)
-       .enter()
-       .append("text")
-       .text(function(d) {
-         return Math.ceil(d.value * 1000) / 1000;
-       })
-       .attr("x", function(d, i) {
-         return i * (timeline_w / dataset.length) + (timeline_w / dataset.length - timeline_barPadding) / 2;
-       })
-       .attr("y", function(d) {
-         return timeline_h - yScale(d.value) + 14;
-       })
-       .attr("font-family", "sans-serif")
-       .attr("font-size", "11px")
-       .attr("fill", "white")
-       .attr("text-anchor", "middle")
-    ;
-
+    drawBarchart(dataset);
   });
 }
+
+function drawBarchart(dataset) {
+  var max = d3.max(dataset, d => d.value);
+  var min = d3.min(dataset, d => d.value);
+  var range = max - min;
+  var minmaxPadding = (range != 0) ? range / 4 : 1.0;
+
+  var yScale = d3.scale.linear()
+    // .domain([min - minmaxPadding, max + minmaxPadding])
+    // .domain([min, max])
+    .domain([0, max])
+    .range([0, timeline_h]);
+
+  svg.selectAll("rect")
+     .data(dataset)
+     .enter()
+     .append("rect")
+     .attr("x", function(d, i) {
+       return i * (timeline_w / dataset.length);
+     })
+     .attr("y", function(d) {
+       return timeline_h - yScale(d.value);
+     })
+     .attr("width", timeline_w / dataset.length - timeline_barPadding)
+     .attr("height", function(d) {
+       return yScale(d.value);
+     })
+     .attr('fill', '#666')
+  ;
+
+  svg.selectAll("text")
+     .data(dataset)
+     .enter()
+     .append("text")
+     .text(function(d) {
+       return Math.ceil(d.value * 1000) / 1000;
+     })
+     .attr("x", function(d, i) {
+       return i * (timeline_w / dataset.length) + (timeline_w / dataset.length - timeline_barPadding) / 2;
+     })
+     .attr("y", function(d) {
+       return timeline_h - yScale(d.value) + 14;
+     })
+     .attr("font-family", "sans-serif")
+     .attr("font-size", "11px")
+     .attr("fill", "white")
+     .attr("text-anchor", "middle")
+  ;
+}
+
